88422811525cf2b502932d61bf3e191391eaa576
rainy25Ghz <1748420480@qq.com>
2021-09-07 19:17:56 +0800

    3

--- src/Posenet/index.tsx
+++ src/Posenet/index.tsx
@@ -5,32 +5,28 @@ import imgsrc from "./images.jpg";
 
 export const Posenet = () => {
   useEffect(() => {
-    // The width and height of the captured photo. We will set the
+  // The width and height of the captured photo. We will set the
   // width to the value defined here, but the height will be
   // calculated based on the aspect ratio of the input stream.
 
-  var width = 320;    // We will scale the photo width to this
-  var height = 0;     // This will be computed based on the input stream
+  const width = 320;    // We will scale the photo width to this
+  let height = 0;     // This will be computed based on the input stream
 
   // |streaming| indicates whether or not we're currently streaming
   // video from the camera. Obviously, we start at false.
 
-  var streaming = false;
+  let streaming = false;
 
   // The various HTML elements we need to configure or control. These
   // will be set by the startup() function.
 
-  var video = null;
-  var canvas = null;
-  var photo = null;
-  var startbutton = null;
+  
 
   function startup() {
-    video = document.getElementById('video');
-    canvas = document.getElementById('canvas');
-    photo = document.getElementById('photo');
-    startbutton = document.getElementById('startbutton');
-
+    console.log('start');
+    let video = document.getElementById('video') as HTMLVideoElement ;
+    let canvas = document.getElementById('canvas') as HTMLCanvasElement;
+   
     navigator.mediaDevices.getUserMedia({video: true, audio: false})
     .then(function(stream) {
       video.srcObject = stream;
@@ -42,6 +38,7 @@ export const Posenet = () => {
 
     video.addEventListener('canplay', function(ev){
       if (!streaming) {
+        console.log('canplay');
         height = video.videoHeight / (video.videoWidth/width);
       
         // Firefox currently has a bug where the height can't be read from
@@ -50,57 +47,14 @@ export const Posenet = () => {
         if (isNaN(height)) {
           height = width / (4/3);
         }
-      
-        video.setAttribute('width', width);
-        video.setAttribute('height', height);
-        canvas.setAttribute('width', width);
-        canvas.setAttribute('height', height);
+        video.width=width;
+        video.height=height;
+        canvas.width=width;
+        canvas.height=height;
         streaming = true;
       }
     }, false);
-
-    // startbutton.addEventListener('click', function(ev){
-    //   takepicture();
-    //   ev.preventDefault();
-    // }, false);
-    
-    // clearphoto();
   }
-
-  // Fill the photo with an indication that none has been
-  // captured.
-
-  // function clearphoto() {
-  //   var context = canvas.getContext('2d');
-  //   context.fillStyle = "#AAA";
-  //   context.fillRect(0, 0, canvas.width, canvas.height);
-
-  //   var data = canvas.toDataURL('image/png');
-  //   photo.setAttribute('src', data);
-  // }
-  
-  // Capture a photo by fetching the current contents of the video
-  // and drawing it into a canvas, then converting that to a PNG
-  // format data URL. By drawing it on an offscreen canvas and then
-  // drawing that to the screen, we can change its size and/or apply
-  // other changes before drawing it.
-
-  // function takepicture() {
-  //   var context = canvas.getContext('2d');
-  //   if (width && height) {
-  //     canvas.width = width;
-  //     canvas.height = height;
-  //     context.drawImage(video, 0, 0, width, height);
-    
-  //     var data = canvas.toDataURL('image/png');
-  //     photo.setAttribute('src', data);
-  //   } else {
-  //     clearphoto();
-  //   }
-  // }
-
-  // Set up our event listener to run the startup process
-  // once loading is complete.
   window.addEventListener('load', startup, false);
   }, []);
   return (
