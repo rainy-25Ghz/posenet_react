17cb38093505790e3e21097c6a4a7585a437f32c
Yana-Hangabina <yan@hongb.in>
2021-09-10 12:10:09 +0800

    fix: alot of bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { Button } from "antd";
 import React, { useState } from "react";
 import "./App.css";
-import { backgroundAudio, draw, pause } from "./Game/Game";
 import { Posenet } from "./Posenet";
 import "antd/dist/antd.css";
 function App() {
@@ -9,24 +7,6 @@ function App() {
   return (
     <div className="App">
       <Posenet setPaused={setpaused}></Posenet>
-      <Button
-        onClick={() => {
-          if (!paused) {
-            pause();
-          } else {
-            if(backgroundAudio){
-              console.log(backgroundAudio)
-              backgroundAudio.autoplay=true;
-              backgroundAudio.play();
-            }
-            requestAnimationFrame(draw);
-          }
-          setpaused(!paused);
-        }}
-      >
-        {paused ? `开始` : `暂停`}
-      </Button>
-    
     </div>
   );
 }
